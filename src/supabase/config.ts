11f05ea3d95443ee2b752930bc283a9cdce3a3e4
NishuJha16 <nishujha088@gmail.com>
2025-07-20 15:28:29 +0530

    fixed keys

--- src/supabase/config.ts
+++ src/supabase/config.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  "https://fliebgbkfepwvyuxruvi.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsaWViZ2JrZmVwd3Z5dXhydXZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3NzMyMjgsImV4cCI6MjA2ODM0OTIyOH0.oda6-za0jHJmjhm7uTHu9WACzkYoVntzBg5h5-AtWis"
-);
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || "";
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || "";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
